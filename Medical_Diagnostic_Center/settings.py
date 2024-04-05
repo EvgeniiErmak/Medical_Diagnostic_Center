e0ed61d06386d9fdad18873614499294dfa28eb4
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-04-05 08:05:19 +0300

    Bootstrap

--- Medical_Diagnostic_Center/settings.py
+++ Medical_Diagnostic_Center/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,9 +42,13 @@ INSTALLED_APPS = [
     "online_consultations",
     "authentication",
     "appointments",
+    "crispy_forms",
     "feedback",
     "clinic",
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -124,9 +129,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
-STATIC_URL = "static/"
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- clinic/views.py
+++ clinic/views.py
@@ -1,3 +1,8 @@
+# clinic/views.py
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    # Возвращает шаблон index.html
+    return render(request, 'index.html')

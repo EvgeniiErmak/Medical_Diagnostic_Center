bb0fba28aa8366a35bef3cca9f5c21c92d7bc73c
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-04-10 11:32:48 +0300

    educational_resources - попытка № 2

--- educational_resources/models.py
+++ educational_resources/models.py
@@ -4,9 +4,12 @@ from django.db import models
 
 
 class Resource(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
+    title = models.CharField(max_length=200)
+    description = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    # Для видео или внешних ссылок можно использовать URLField
+    video_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.title
--- educational_resources/urls.py
+++ educational_resources/urls.py
@@ -1,16 +1,11 @@
 # educational_resources/urls.py
 
 from django.urls import path
-from . import views
+from .views import resources_list, resource_detail
 
 app_name = 'educational_resources'
 
 urlpatterns = [
-    path('resources/', views.resources_list, name='resources_list'),
-    path('resources/<int:pk>/', views.resource_detail, name='resource_detail'),
-    path('resources/1/', views.resource_detail_1, name='resource_detail_1'),
-    path('resources/2/', views.resource_detail_2, name='resource_detail_2'),
-    path('resources/3/', views.resource_detail_3, name='resource_detail_3'),
-    path('resources/4/', views.resource_detail_4, name='resource_detail_4'),
-    path('resources/5/', views.resource_detail_5, name='resource_detail_5'),
+    path('', resources_list, name='resources_list'),
+    path('<int:pk>/', resource_detail, name='resource_detail'),
 ]

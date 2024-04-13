2061a800eda84ce3214a08c6eceb0e10447f555b
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-04-13 12:22:07 +0300

    Запись на прием онлайн - 2.0

--- appointments/models.py
+++ appointments/models.py
@@ -1,17 +1,18 @@
 # appointments/models.py
 
-from django.conf import settings
 from django.db import models
+from django.conf import settings
+from clinic.models import Specialist
 
 
 class AppointmentSlot(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    doctor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='appointment_slots')
+    specialist = models.ForeignKey(Specialist, on_delete=models.CASCADE, related_name='appointment_slots', null=True)  # временно разрешаем null
     is_booked = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.doctor}'s slot from {self.start_time} to {self.end_time}"
+        return f"{self.specialist}'s slot from {self.start_time} to {self.end_time}"
 
 
 class Appointment(models.Model):
--- appointments/urls.py
+++ appointments/urls.py
@@ -1,12 +1,13 @@
 # appointments/urls.py
 
 from django.urls import path
+from .views import book_appointment
 from . import views
 
 app_name = 'appointments'
 
 urlpatterns = [
     path('list/', views.appointments_list, name='appointments_list'),
-    path('book/<int:slot_id>/', views.book_appointment, name='book_appointment'),
+    path('book/<int:specialist_id>/', book_appointment, name='book_appointment'),
     path('view/', views.view_appointments, name='view_appointments'),
 ]
